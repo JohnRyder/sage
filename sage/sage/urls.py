9cc59c2c2c08db69f5e7cfc7f6729851b02044b2
j_ryder <john.ryder1988@gmail.com>
2015-01-05 15:06:31 +0000

    Added accounts viewset to urls.py

--- sage/sage/urls.py
+++ sage/sage/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from rest_framework import routers
+
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sage.views.home', name='home'),
@@ -8,4 +15,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^authentication/', include('allaccess.urls')),
+    url(r'^api/v1/', include(router.urls))
+    #url('^.*$', IndexView.as_view(), name='index'),
 )
